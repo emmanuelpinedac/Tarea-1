461cfa45df81c3c85bdb312085114f6e3add4fd5
emmanuelpinedac <emmanuelpinedac@outlook.com>
2022-09-28 00:30:47 -0600

    "update"

--- Extra 2.js
+++ Extra 2.js
@@ -2,15 +2,10 @@
 
 //
 function palindromo(str) {
-    const strReverso = str.split("").reverse().join("");
-  
-    return strReverso === str ? " si es palindromo" : "no es palindromo";
-  }
-  console.log(palindromo("ana")); // si es palindromo
-  console.log(palindromo("hola")); // no es palindromo
-  console.log(palindromo("omo")); // si es palindromo
+  const strReverso = str.split("").reverse().join("");
 
-
-
-  
-  
+  return strReverso === str ? " si es palindromo" : "no es palindromo";
+}
+console.log(palindromo("ana")); // si es palindromo
+console.log(palindromo("hola")); // no es palindromo
+console.log(palindromo("oso")); // si es palindromo
